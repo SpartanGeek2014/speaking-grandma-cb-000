6ca9a051e19442f3f56b8ca80a7eded98bc033ca
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-14 14:43:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,8 +19,12 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
 
-  else
-    return "NO, NOT SINCE 1938!"
+    elsif     
+      return "NO, NOT SINCE 1938!"
 
+    else
+      return "I LOVE YOU TOO PUMPKIN!"
+
+    end
   end
 end
